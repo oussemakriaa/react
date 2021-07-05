0dd99389df411d9eda18b94ca90b19f7ec306b43
oussemakriaa <oussemakriaa32@gmail.com>
2021-07-05 17:18:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formul from './components/Formul';
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Formul />
+    </div>
+  );
+}
+
+export default App;
